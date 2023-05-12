6051466d1c9f4c129fd2f1026b3ac9f70cd204b1
thisisbillall <mdbilal7620@gmail.com>
2023-05-12 19:07:42 +0530

    Node server

---
+++ server/Models/middlemen.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const registerSchema = mongoose.Schema({
+  uid: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  walladdr: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Register", registerSchema);
