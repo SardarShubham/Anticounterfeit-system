6c075ed5e0ff9c270349dc930cbbe9b835dd3bd2
thisisbillall <mdbilal7620@gmail.com>
2023-05-12 19:44:28 +0530

    mongo reg

--- server/Models/middlemen.js
+++ server/Models/middlemen.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const registerSchema = mongoose.Schema({
-  uid: {
-    type: String,
-    required: true,
-  },
+  // uid: {
+  //   type: String,
+  //   required: true,
+  // },
   name: {
     type: String,
     required: true,
