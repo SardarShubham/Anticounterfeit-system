29f4c750a5ee37d0057576d46643eb81e4f985c6
sardarshubham <sardarshubham17@gmail.com>
2023-05-13 16:54:59 +0530

    integration functions written in connect.js

--- client/src/connect.js
+++ client/src/connect.js
@@ -18,156 +18,193 @@ export const isWalletExist = async () => {
   return false;
 };
 
-  export const fetchDetails = async (_id, _callback) => {
-    // _id = "0x8b85578df4f8ccad6d617eea0907c7bba879b1dda139df26c351b8f432a32604";
-    console.log(_id);
-    _id = "0xd0431b44d3911afee99f5fe6774d8e5ab50d24030cb04d45a60597cbf09f9696"
-    const wallet = await isWalletExist();
-    if (wallet) {
-      const accs = await window.ethereum.enable();
-      const acc = accs[0];
-      sampleContract.methods
-        .get_details(_id)
-        .estimateGas()
-        .then(gas => {
-          return sampleContract.methods
-          .get_details(_id)
-          .send({
-              from: acc,
-              gas,
-            })
-            .then(res => {
-              console.log(res.events.details.returnValues.ret_data);
-              _callback(res.events.details.returnValues.ret_data);
-            })
-            .catch((err) => {
-              console.log(err);
-              alert("Something went wrong", err);
-            });
-        })
-        .catch((err) => {
-          console.log(err);
-          alert("Something went wrong", err);
-        });
-    }
-  };
-export const create = async (name, accAddress, exp_date, mrp, packOf, location) => {
-  // dummy data
-  let _name, _retailerName, _retialerAddress, _expiry, _price, _packof, _location;
-  _name = "Oppo Mobile";
-  _retailerName = "Dhankawadi Electronics";
-  _retialerAddress = "0x583031D1113aD414F02576BD6afaBfb302140225";
-  _expiry = 0;
-  _price = 4999;
-  _packof = 1;
-  _location = "Banglore";
 
-  console.log(name, accAddress, exp_date, mrp, packOf, location);
+// get individual item details from QR id.
+export const getItem = async (_id, _callback) => {
+  // _id = "0x8b85578df4f8ccad6d617eea0907c7bba879b1dda139df26c351b8f432a32604";
+  console.log(_id);
+  // _id = "0xd0431b44d3911afee99f5fe6774d8e5ab50d24030cb04d45a60597cbf09f9696"
   const wallet = await isWalletExist();
   if (wallet) {
     const accs = await window.ethereum.enable();
-    let acc = accs[0];
-    console.log(typeof accAddress, accAddress);
-
-    // let _gas = await sampleContract.methods
-    //   .create(_name, _retailerName, _retialerAddress, _expiry, _price, _packof, _location)
-    //   .estimateGas();
-    // console.log("gas", _gas);
-    // let _gas = 99999;
-
-    return sampleContract.methods
-      .create(_name, _retailerName, _retialerAddress, _expiry, _price, _packof, _location)
-      .send({
-        from: acc,
-        gas:'1000000',
+    const acc = accs[0];
+    sampleContract.methods
+      .get_details(_id)
+      .estimateGas()
+      .then(gas => {
+        return sampleContract.methods
+          .get_details(_id)
+          .send({
+            from: acc,
+            gas,
+          })
+          .then(res => {
+            console.log(res.events.details.returnValues.ret_data);
+            _callback(res.events.details.returnValues.ret_data);
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("Something went wrong", err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something went wrong", err);
       });
   }
 };
 
-export const remove = async (id) => {
+
+// creating new product by the manufacturer
+export const createItem = async (name, manuName, retailerName, retailerAddr, mrp, packOf, location, _callback) => {
+  // dummy data
+  // let _name, _retailerName, _retialerAddress, _expiry, _price, _packof, _location;
+  // _name = "Oppo Mobile";
+  // _retailerName = "Dhankawadi Electronics";
+  // _retialerAddress = "0x583031D1113aD414F02576BD6afaBfb302140225";
+  // _expiry = 0;
+  // _price = 4999;
+  // _packof = 1;
+  // _location = "Banglore";
+
+  console.log(name, manuName, retailerName, retailerAddr, mrp, packOf, location);
   const wallet = await isWalletExist();
   if (wallet) {
     const accs = await window.ethereum.enable();
     const acc = accs[0];
-    let gas = await sampleContract.methods.remove(id).estimateGas();
-    console.log("gas", gas);
-
-    return sampleContract.methods.remove(id).send({
-      from: acc,
-      gas,
-    });
+    sampleContract.methods
+      .create(name, manuName, retailerName, retailerAddr, mrp, packOf, location)
+      .estimateGas()
+      .then(gas => {
+        return sampleContract.methods
+          .create(name, manuName, retailerName, retailerAddr, mrp, packOf, location)
+          .send({
+            from: acc,
+            gas,
+          })
+          .then(res => {
+            console.log(res.events.details.returnValues);
+            _callback(res.events.details.returnValues);
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("Something went wrong", err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something went wrong", err);
+      });
   }
 };
 
-export const verifyAndNext = async(prodID, nextAddr ,loc)=>{
-  // prodID = "0x8b85578df4f8ccad6d617eea0907c7bba879b1dda139df26c351b8f432a32604";
-  // nextAddr = "0x9573c561cae253dAb2cf64f0F993252Ea66300F3";
-  // loc = "D";
-  const wallet = await isWalletExist();
-  if (wallet) {
-    const accs = await window.ethereum.enable();
-    const acc = accs[0];
-    owner = acc;
-    console.log(prodID, nextAddr, loc)
-    // let gas = await sampleContract.methods.next_location(prodID, nextAddr, loc).estimateGas();
-    let gas = 99999;
-    console.log("gas", gas);
-    console.log("ow", owner)
-
-    console.log(prodID, nextAddr, loc)
-    console.log(typeof(prodID));
-    console.log(typeof(nextAddr));
-    console.log(typeof(loc));
-    // let gas = await sampleContract.methods.next_location(
-    //   prodID,
-    //   nextAddr, 
-    //   loc
-    //    ).estimateGas();
-    // let gas = 99999;
-    console.log("gas", gas);
-    return sampleContract.methods.next_location(
-      prodID,
-      nextAddr, 
-      loc
-      ).send({
-      from: acc,
-      gas,
-    });
-
-    // let g = await sampleContract.methods.verify_user(prodID)
-    // .estimateGas();
-    // console.log(g);
-    // return sampleContract.methods.verify_user(prodID)
-    // .send({
-    //   from : acc,
-    //   g
-    // });
-  }
-
-};
-
-
-
-
-
-// export const addNext = async(prodID, next_addr)=>{
-
+// export const remove = async (id) => {
 //   const wallet = await isWalletExist();
 //   if (wallet) {
 //     const accs = await window.ethereum.enable();
 //     const acc = accs[0];
-//     owner = acc;
-//     let gas = await sampleContract.methods.verify_user(prodID, next_addr).estimateGas();
+//     let gas = await sampleContract.methods.remove(id).estimateGas();
 //     console.log("gas", gas);
-//     console.log("ow", owner)
-//     sampleContract.methods.verify_user(prodID, next_addr).send({
+
+//     return sampleContract.methods.remove(id).send({
 //       from: acc,
 //       gas,
-//     })
-//     .then((res)=>{
-//      console.log(res)
-//     }).catch((err)=>{
-//       console.log(err)
-//     })
+//     });
 //   }
 // };
+
+// for intermediate, make next transaction
+export const verifyAndNext = async (prodID, int_name, nextAddr, loc, _callback) => {
+  const wallet = await isWalletExist();
+  if (wallet) {
+    const accs = await window.ethereum.enable();
+    const acc = accs[0];
+    sampleContract.methods
+      .next_location(prodID, int_name, nextAddr, loc)
+      .estimateGas()
+      .then(gas => {
+        return sampleContract.methods
+          .next_location(prodID, int_name, nextAddr, loc)
+          .send({
+            from: acc,
+            gas,
+          })
+          .then(res => {
+            console.log(res.events.details.returnValues);
+            _callback(res.events.details.returnValues);
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("Something went wrong", err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something went wrong", err);
+      });
+  }
+};
+
+// to fetch all the products in the blockchain
+export const getAllItems = async(_callback) => {
+  const wallet = await isWalletExist();
+  if (wallet) {
+    const accs = await window.ethereum.enable();
+    const acc = accs[0];
+    sampleContract.methods
+      .get_all()
+      .estimateGas()
+      .then(gas => {
+        return sampleContract.methods
+          .get_all()
+          .send({
+            from: acc,
+            gas,
+          })
+          .then(res => {
+            console.log(res.events.details.returnValues);
+            _callback(res.events.details.returnValues);
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("Something went wrong", err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something went wrong", err);
+      });
+  } 
+}
+
+export const sellItem = async(_id, _callback) => {
+  const wallet = await isWalletExist();
+  if (wallet) {
+    const accs = await window.ethereum.enable();
+    const acc = accs[0];
+    sampleContract.methods
+      .invalidate(_id)
+      .estimateGas()
+      .then(gas => {
+        return sampleContract.methods
+          .invalidate(_id)
+          .send({
+            from: acc,
+            gas,
+          })
+          .then(res => {
+            console.log(res.events.details.returnValues);
+            _callback(res.events.details.returnValues);
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("Something went wrong", err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something went wrong", err);
+      });
+  } 
+}
+
